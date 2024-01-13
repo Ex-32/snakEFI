076d656f5a26f4397674fca8963143d0bc637223
Ex-32 <jenna@fligor.net>
2024-01-13 05:51:34 -0600

    major rework of nix flake (now supports running in a VM)

--- flake.nix
+++ flake.nix
@@ -1,38 +1,78 @@
 {
   description = "Example cmake/c++ flake";
   
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { self, nixpkgs }: let
-    # supported systems for this package
-    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    # Nixpkgs instantiated for supported system types.
-    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-  in {
-    packages =  forAllSystems (system: let 
-      pkgs = nixpkgsFor.${system};
-    in {
-      default = pkgs.stdenv.mkDerivation {
-        pname = "snakEFI";
-        version = "1.0.1";
-        src = self;
-        # we're doing some seriously questionably cross-compilation shenanigans
-        # and nix's normally great security practices just breaks things.
-        hardeningDisable = [ "pic" "stackprotector" ];
-        installPhase = ''
-          mkdir -p $out/share
-          cp BOOTX64.EFI $out/share
-        '';
-        buildInputs = with pkgs; [
-          llvmPackages_16.clang
-          lld_16
-          gnu-efi
-        ];
-      };
-    });
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    default-systems.url = "github:nix-systems/default";
   };
+
+  outputs = inputs @ { self, nixpkgs, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = (import inputs.default-systems);
+      perSystem = { pkgs, ... }: { 
+        packages = let 
+          name = "snakEFI";
+          version = "1.0.2";
+        in rec {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "${name}";
+            inherit version;
+            src = self;
+            # we're doing some seriously questionable cross-compilation shenanigans
+            # and nix's normally great security practices just breaks things.
+            hardeningDisable = [ "pic" "stackprotector" ];
+            installPhase = ''
+              mkdir -p $out/share
+              cp BOOTX64.EFI $out/share
+            '';
+            buildInputs = with pkgs; [
+              gnu-efi
+              lld_16
+              llvmPackages_16.clang
+            ];
+          };
+          image = pkgs.stdenv.mkDerivation {
+            pname = "${name}-vm-image";
+            inherit version;
+            # this is a no-op for unpack, since this derivation has no sources
+            unpackPhase = "true";
+            buildPhase = ''
+              dd if=/dev/zero of=snakEFI.img bs=1k count=1440
+              mformat -i snakEFI.img -f 1440 ::
+              mmd -i snakEFI.img ::/EFI
+              mmd -i snakEFI.img ::/EFI/BOOT 
+              mcopy -o -s -i snakEFI.img ${default}/share/BOOTX64.EFI ::/EFI/BOOT
+            '';
+            installPhase = ''
+              mkdir -p $out/share
+              cp snakEFI.img $out/share
+            '';
+            nativeBuildInputs = [
+              pkgs.mtools 
+              default
+            ];
+          };
+          vm = pkgs.writeShellApplication {
+            name = "${name}-vm-wrapper";
+            runtimeInputs = [
+              pkgs.OVMF
+              pkgs.qemu
+              default
+              image
+            ];
+            text = ''
+              cd "$(realpath "$(dirname "$0")")"
+              IMG_TMP="$(mktemp -d)"
+              trap 'rm -rf "$IMG_TMP"' EXIT
+              cp --no-preserve=mode,ownership ${image}/share/snakEFI.img "$IMG_TMP"
+              cp --no-preserve=mode,ownership ${pkgs.OVMF.fd}/FV/OVMF_CODE.fd "$IMG_TMP"
+              ${pkgs.qemu}/bin/qemu-system-x86_64 \
+                -drive if=pflash,format=raw,file="$IMG_TMP/OVMF_CODE.fd" \
+                -drive format=raw,file="$IMG_TMP/snakEFI.img"
+            '';
+          };
+        };
+      };
+    };
 }
